65384f20c2b49ff1023380cfc9ec7891dcc92bc0
mdshorif-islam <imdshorif256@gmailcom>
2024-02-27 19:41:44 +0600

    update02

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const apiKey = "8c1cdae51f294825c041a70e2cc6b9bc";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed +  " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        
+    }
+    
+
+}
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
