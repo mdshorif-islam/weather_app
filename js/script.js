cd52e4ca8e26c82dd50621bb9ad1bd75a86d056b
mdshorif-islam <imdshorif256@gmailcom>
2024-02-29 02:53:00 +0600

    full complet

--- js/script.js
+++ js/script.js
@@ -3,40 +3,41 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const watherIcon = document.querySelector(".wather-icon");
+const weatherIcon = document.querySelector(".weather-icon");
 
-
-async function checkWeather(city){
+async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
-    var data = await response.json();
+    const data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-    document.querySelector(".wind").innerHTML = data.wind.speed +  " km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    if(data.weather[0].main == "Clouds"){
-        watherIcon.src = "images/clouds.png";
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
     }
-    else if(data.weather[0].main == "Clear"){
-        watherIcon.src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        watherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        watherIcon.src = "images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-        watherIcon.src = "images/mist.png";
-    }
-    
+
     document.querySelector(".weather").style.display = "block"
-
 }
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-})
 
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+// Add event listener for Enter key press
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
